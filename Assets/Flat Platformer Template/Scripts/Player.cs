3862b0a7fafe22e8c1e864475a73479d15f51f62
barshaid <exofang@hotmail.com>
2019-12-15 21:44:04 +0200

    init

---
+++ Assets/Flat Platformer Template/Scripts/Player.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour {
+    public float WalkSpeed;
+    public float JumpForce;
+    public AnimationClip _walk, _jump;
+    public Animation _Legs;
+    public Transform _Blade, _GroundCast;
+    public Camera cam;
+    public bool mirror;
+
+
+    private bool _canJump, _canWalk;
+    private bool _isWalk, _isJump;
+    private float rot, _startScale;
+    private Rigidbody2D rig;
+    private Vector2 _inputAxis;
+    private RaycastHit2D _hit;
+
+	void Start ()
+    {
+        rig = gameObject.GetComponent<Rigidbody2D>();
+        _startScale = transform.localScale.x;
+	}
+
+    void Update()
+    {
+        
+        Vector3 myScale = transform.localScale;
+        bool rotate = false;
+
+        if (cam.ScreenToWorldPoint(Input.mousePosition).x > transform.position.x + 0.2f)
+            mirror = false;
+        if (cam.ScreenToWorldPoint(Input.mousePosition).x < transform.position.x - 0.2f)
+            mirror = true;
+
+        if (Input.GetAxis("Horizontal") < 0 && myScale.x > 0)
+        {
+            rotate = true;
+
+        }
+        else if (Input.GetAxis("Horizontal") > 0 && myScale.x < 0)
+        {
+            rotate = true;
+        }
+        if (rotate)
+        {
+            myScale.x = myScale.x * -1;
+            transform.localScale = myScale;
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            GetComponent<Animation>().Play();
+        }
+
+        if (_hit = Physics2D.Linecast(new Vector2(_GroundCast.position.x, _GroundCast.position.y + 0.2f), _GroundCast.position))
+        {
+            if (!_hit.transform.CompareTag("Player"))
+            {
+                _canJump = true;
+                _canWalk = true;
+            }
+        }
+        else _canJump = false;
+
+        _inputAxis = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (_inputAxis.y > 0 && _canJump)
+        {
+            _canWalk = false;
+            _isJump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
+
+        if (cam.ScreenToWorldPoint(Input.mousePosition).x > transform.position.x + 0.2f)
+            mirror = false;
+        if (cam.ScreenToWorldPoint(Input.mousePosition).x < transform.position.x - 0.2f)
+            mirror = true;
+
+      
+
+        if (_inputAxis.x != 0)
+        {
+            rig.velocity = new Vector2(_inputAxis.x * WalkSpeed * Time.deltaTime, rig.velocity.y);
+
+            if (_canWalk)
+            {
+                _Legs.clip = _walk;
+                _Legs.Play();
+            }
+        }
+
+        else
+        {
+            rig.velocity = new Vector2(0, rig.velocity.y);
+        }
+
+        if (_isJump)
+        {
+            rig.AddForce(new Vector2(0, JumpForce));
+            _Legs.clip = _jump;
+            _Legs.Play();
+            _canJump = false;
+            _isJump = false;
+        }
+    }
+
+    public bool IsMirror()
+    {
+        return mirror;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, _GroundCast.position);
+    }
+}
